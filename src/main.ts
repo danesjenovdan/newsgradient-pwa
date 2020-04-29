29d6725dfc7c5c453f2b9fb5316263d3bbf61562
LepkoQQ <lepko.san@gmail.com>
2020-04-29 15:19:41 +0200

    fix warnings in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import './registerServiceWorker';
-import store from './store';
 import Vue2TouchEvents from 'vue2-touch-events';
 import VueMatomo from 'vue-matomo';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
 
 Vue.use(Vue2TouchEvents, {
   swipeTolerance: 100,
