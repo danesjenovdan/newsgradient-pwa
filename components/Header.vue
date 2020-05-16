151e4fac0061fc210db0ab61e69b1d7384a1ba75
Aljaz Ferencek <aljaz@desetka.si>
2020-05-16 20:54:52 +0200

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="ng-header">
+    <div class="container">
+      <div class="flex-row">
+        <router-link to="/" class="logo-link">
+          <h1>NEWSGRADIENT</h1>
+        </router-link>
+        <h2>
+          5 most reported events by Bosnian<br />
+          press sorted by the partisan bias.
+        </h2>
+        <nav>
+          <ul>
+            <li><router-link exact to="/">Events</router-link></li>
+            <li><router-link exact to="/about">About</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+#ng-header {
+  background-color: #fff;
+  top: 0;
+  left: 0;
+  width: 100%;
+  box-shadow: 0px -10px 20px 10px rgba(0, 0, 0, 0.25);
+  padding: 1.5rem 6rem;
+  .flex-row {
+    display: flex;
+    align-items: center;
+    .logo-link {
+      text-decoration: none;
+      display: block;
+      h1 {
+        margin: 0;
+        font-size: 2.5rem;
+        font-weight: 700;
+        color: transparent;
+        background-image: url('../assets/logo-text.svg');
+        background-repeat: no-repeat;
+        background-position: left center;
+        background-size: 10em 100%;
+        letter-spacing: 0.175em;
+      }
+    }
+    h2 {
+      margin: 0;
+      font-size: 1rem;
+      font-weight: 300;
+      font-style: italic;
+      line-height: 1.2;
+      margin-left: 0.5rem;
+    }
+    nav {
+      margin-left: auto;
+      ul {
+        list-style: none;
+        display: flex;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        li {
+          margin-right: 4rem;
+          &:last-child {
+            margin-right: 0;
+          }
+          a {
+            text-decoration: none;
+            color: inherit;
+            font-size: 27px;
+            font-weight: 400;
+            line-height: 1;
+            &:hover {
+              text-decoration: underline;
+            }
+            &.router-link-active {
+              text-decoration: underline;
+              color: #e00;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="section-title">
+        <p>Trump’s impeachment speech comments</p>
+      </div>
+      <div class="section-line"></div>
+      <div class="row">
+        <div class="column">
+          <div class="side-column">
+            Some Text in Column One
+          </div>
+        </div>
+        <div class="column">
+          <div class="main-column">
+            Some Text in Column Two
+          </div>
+        </div>
+        <div class="column">
+          <div class="side-column">
+            Some Text in Column Three
+          </div>
+        </div>
+      </div>
+    </div>
+    <Selector />
+  </div>
+</template>
+
+<style>
+.section-title {
+  text-align: center;
+  margin: 1.5rem 0;
+  font-size: 40px;
+  font-weight: 400;
+  line-height: 35px;
+}
+.section-line {
+  width: 1923px;
+  height: 5px;
+  background-image: linear-gradient(to right, #0076fe 0%, #e50001 100%);
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  flex: 1;
+}
+</style>
+<script>
+import Selector from '../components/Selector'
+export default {
+  components: { Selector }
+}
+</script>
