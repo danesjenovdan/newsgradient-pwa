6d2f757a9c2377b9a30fdcc7dd4ac63ddeeabfb4
Aljaz Ferencek <aljaz@desetka.si>
2020-05-16 21:28:29 +0200

    Commit with carousel

--- components/Selector.vue
+++ components/Selector.vue
@@ -35,52 +35,60 @@ export default {
   position: fixed;
   bottom: 0;
   width: 100%;
+  background-color: white;
   box-shadow: 0px -10px 20px 0px rgba(0, 0, 0, 0.25);
   padding: 1.5rem 0;
 }
+
 .choice-bar {
   margin: 0 20%;
-}
-.choice-bar li {
-  list-style-type: none;
-  width: 20%;
-  float: left;
-  font-size: 12px;
-  position: relative;
-  text-align: center;
-  text-transform: uppercase;
-  color: #7d7d7d;
-}
-.choice-bar li:before {
-  width: 60px;
-  height: 60px;
-  content: '';
-  line-height: 30px;
-  border: 7px solid #dddddd;
-  background-color: #dddddd;
-  display: block;
-  text-align: center;
-  margin: 0 auto 10px auto;
-  border-radius: 50%;
-}
-.choice-bar li:after {
-  width: 100%;
-  height: 7px;
-  content: '';
-  position: absolute;
-  background-color: #dddddd;
-  top: 25px;
-  left: -50%;
-  z-index: -1;
-}
-.choice-bar li:first-child:after {
-  content: none;
-}
-.choice-bar li.active:before {
-  border-color: #dddddd;
-  background-color: #0177ff;
-}
-.choice-bar li.active + li:after {
-  background-color: #dddddd;
+
+  li {
+    list-style-type: none;
+    width: 20%;
+    float: left;
+    font-size: 12px;
+    position: relative;
+    text-align: center;
+    text-transform: uppercase;
+    color: #7d7d7d;
+  }
+
+  li:before {
+    width: 60px;
+    height: 60px;
+    content: '';
+    line-height: 30px;
+    border: 7px solid #dddddd;
+    background-color: #dddddd;
+    display: block;
+    text-align: center;
+    margin: 0 auto 10px auto;
+    border-radius: 50%;
+  }
+
+  li:after {
+    width: 100%;
+    height: 7px;
+    content: '';
+    position: absolute;
+    background-color: #dddddd;
+    top: 25px;
+    left: -50%;
+    z-index: -1;
+  }
+
+  li:first-child:after {
+    content: none;
+  }
+
+  li.active:before {
+    border-color: #dddddd;
+    background-color: #0177ff;
+  }
+
+  li.active + li:after {
+    background-color: #dddddd;
+  }
 }
 </style>
